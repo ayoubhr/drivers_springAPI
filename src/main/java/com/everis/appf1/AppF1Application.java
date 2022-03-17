c985597fa3f50c0c2776d8a82831b72e29508412
Ayoub Hiar Boumhamdi <ahiarbou@emeal.nttdata.com>
2022-03-17 16:30:44 +0100

    testing out

--- src/main/java/com/everis/appf1/AppF1Application.java
+++ src/main/java/com/everis/appf1/AppF1Application.java
@@ -12,14 +12,4 @@ public class AppF1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(AppF1Application.class, args);
 	}
-
-	/*@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-	
-	@GetMapping("/")
-	public String error() {
-		return String.format("Error page");
-	}*/
 }
